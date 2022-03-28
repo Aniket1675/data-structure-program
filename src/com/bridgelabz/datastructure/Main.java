48d061a53900146685d0765dfb113288dfdd43dd
Aniket1675 <Aniket@05>
2022-03-28 23:30:45 +0530

    addding stack operations

--- src/com/bridgelabz/datastructure/Main.java
+++ src/com/bridgelabz/datastructure/Main.java
@@ -3,6 +3,7 @@ package com.bridgelabz.datastructure;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Welcome to Linkedlist Program");
 	 LinkedList linkedList = new LinkedList<>();
         linkedList.addFirst(10);
         linkedList.addLast(12);
@@ -22,6 +23,24 @@ public class Main {
 
         linkedList.deleteLast();
         linkedList.printList();
+
+        System.out.println("Welcome to Stacks Program");
+
+
+        Stack stack = new Stack();
+        stack.push("10");
+        stack.push("20");
+        stack.push("30");
+        stack.push("40");
+        stack.showElements();
+
+        stack.pop();
+        stack.showElements();
+        stack.pop();
+        stack.showElements();
+        stack.pop();
+        stack.showElements();
+        System.out.println("----------------");
 
 
     }
