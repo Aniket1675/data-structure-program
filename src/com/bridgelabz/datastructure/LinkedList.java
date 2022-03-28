af4ddb3da68988a7b4698c9d65c9c86bc3eb285f
Aniket1675 <Aniket@05>
2022-03-28 23:04:37 +0530

    added printList() for display the linkedlist

--- src/com/bridgelabz/datastructure/LinkedList.java
+++ src/com/bridgelabz/datastructure/LinkedList.java
@@ -47,4 +47,17 @@ public class LinkedList <T> {
         currNode.next = newNode;
 
     }
+    public void printList(){
+        if(head == null){
+            System.out.println("list is empty");
+            return;
+        }
+
+        Node currNode = head;
+        while(currNode != null) {
+            System.out.print(currNode.data  + "->");
+            currNode = currNode.next;
+        }
+        System.out.println("NULL");
+    }
 }
