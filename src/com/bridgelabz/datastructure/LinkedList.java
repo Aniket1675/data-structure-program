bd80b40238e5ea97dd10e1aba9db5e7ed72da23e
Aniket1675 <Aniket@05>
2022-03-28 18:49:40 +0530

    Added node class in linkedlist program

--- src/com/bridgelabz/datastructure/LinkedList.java
+++ src/com/bridgelabz/datastructure/LinkedList.java
@@ -1,2 +1,24 @@
-package com.bridgelabz.datastructure;public class LinkedList {
+package com.bridgelabz.datastructure;
+
+public class LinkedList <T> {
+    Node head; //creating the head node
+    public int size;
+
+
+    LinkedList() {
+        this.size = 0;  //initializing the size as zero
+    }
+
+    //Node class
+    public class Node<T> {
+        T data;
+        Node next;
+
+        //Constructor
+        Node(T data) {
+            this.data = data;
+            this.next = null; //for every new node its next will be null by default
+            size++;  //do size++ when creating the new node
+        }
+    }
 }
